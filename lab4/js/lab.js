0ecb3b140d7338be456982f34bc82c6137568027
elomath <elomath@csumb.edu>
2021-02-10 10:11:41 -0800

    lab4_update

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,3 +1,12 @@
+//you get a deep sauce pan
+//get eggs and put them in the sauce pan
+//you then fill it with water until the eggs are submerged
+//turn on the stove to a high medium heat
+//let boil for 10-12 minutes
+//turn off stove and run eggs under cold water
+//peel under cold water
+//they are ready to eat
+
 //display maze path
 //detect player input
 //move based on input
