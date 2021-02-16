7695334d67282a4063c3849192dfa2e617fcc699
elomath <elomath@csumb.edu>
2021-02-16 10:47:40 -0800

    lab5_initial

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Everett Lomath
+ * Created:   2/14/2021
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+ // Define Variables
+ make = "Chevy";
+ model = "Camero";
+ color = "yellow";
+ year = 2007;
+
+ // Calculate
+ age = 2021 - year;
+
+ // output
+ document.writeIN("Make:" + make +"<br>"):
+ document.writeIN("Model:" + model +"<br>"):
+ document.writeIN("Color:" + color +"<br>"):
+ document.writeIN("Year:" + year +"<br>"):
+ document.writeIN("Age:" + age +"<br>"):
