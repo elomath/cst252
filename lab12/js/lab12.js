f93fe83fbf73c9e8d5d24f1a9d34eb228306a1f9
elomath <elomath@csumb.edu>
2021-03-13 17:05:52 -0800

    lab12_start

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -10,14 +10,25 @@
 
  //For numbers which are multiples of both 3 and 5, print “FizzBuzz”
 
-for (i=1; i <= 100; i++) {
-    if (i%15 == 0) {
-       console.log("FizzBuzz");
+for (i=1; i <= 200; i++) {
+    if (i%35 == 0) {
+       console.log("BoomFizz");
     } else if (i%3 == 0) {
         console.log("Buzz");
     } else if (i%5 == 0) {
         console.log("Fizz");
+    } else if (i%7 == 0) {
+        console.log("Boom");
+    } else if (i%15 == 0) {
+        console.log("FizzBuzz");
+    } else if (i%21 == 0) {
+        console.log("BoomBuzz");
     } else {
       console.log(i);
     }
 }
+
+str += "Fizz";
+var newEl = document.createElement("p");
+newEl.innerHTML = str;
+document.getElementById("output").appendChild(newEl);
